9406d3706e7a4232d46ab7d1f5daa9cfdb054cd1
unknown <ruben.pypoa@gmail.com>
2026-03-28 16:32:33 +0100

    Commited

--- cyber-assessment/1-positive_or_negative.py
+++ cyber-assessment/1-positive_or_negative.py
@@ -2,10 +2,10 @@
 import random
 number = random.randint(-10, 10)
 if number > 0:
-	print(f"{number} positive")
-else number < 0:
-	print(f"{number} negative")
-else number == 0:
-	print(f"{number} zero")
+	print(f"{number} is positive")
+elif number < 0:
+	print(f"{number} is negative")
+elif number == 0:
+	print(f"{number} is zero")
 else:
 	print(f"wrong type")
