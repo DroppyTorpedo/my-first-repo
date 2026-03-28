f6edef1db8f57b92638d0351e5633d98b1ff4aff
unknown <ruben.pypoa@gmail.com>
2026-03-28 16:37:45 +0100

    Second Commit

--- cyber-assessment/2-print_comb2.py
+++ cyber-assessment/2-print_comb2.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 for i in range(100):
-    print(f"{i:02d}", end="\n" if i == 99 else ", ")
+    if i < 99:
+        print(f"{i:02d}", end=", ")
+    else:
+        print(f"{i:02d}")
